0232dd05aa6dd80102ed7e2249aaca9274d59480
Jay Pilgreen <jpilgreen@kcstar.com>
2016-07-05 16:52:58 -0500

    style changes and cleanup

--- js/load.js
+++
@@ -1,8 +0,0 @@
-(function() {
-  // Load Web Components
-  if(!window.WebComponents) {
-    var script = document.createElement('script');
-    script.src = 'https://cdnjs.cloudflare.com/ajax/libs/webcomponentsjs/0.7.22/webcomponents-lite.min.js';
-    document.head.appendChild(script);
-  }
-})();
